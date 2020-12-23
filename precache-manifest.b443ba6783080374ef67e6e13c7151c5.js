8870642da7fbd7d7eb95d2218208dbd3fcfddde5
Giovanni <GiovanniGiampaolo@users.noreply.github.com>
2020-12-23 15:28:34 +0100

    Updates

---
+++ precache-manifest.b443ba6783080374ef67e6e13c7151c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253209b9a760547c3a33d0d3bb811017",
+    "url": "/list-order-app/index.html"
+  },
+  {
+    "revision": "eda37dba00d386456a53",
+    "url": "/list-order-app/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "aad57cc1040e1edac446",
+    "url": "/list-order-app/static/css/main.0b0cb9d5.chunk.css"
+  },
+  {
+    "revision": "eda37dba00d386456a53",
+    "url": "/list-order-app/static/js/4.b333aec5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/list-order-app/static/js/4.b333aec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c901484a65959d8b60",
+    "url": "/list-order-app/static/js/5.47577fb7.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/list-order-app/static/js/5.47577fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655fa2cfa68aa051452c",
+    "url": "/list-order-app/static/js/6.25beb86d.chunk.js"
+  },
+  {
+    "revision": "aad57cc1040e1edac446",
+    "url": "/list-order-app/static/js/main.978d825c.chunk.js"
+  },
+  {
+    "revision": "d9103ab4e4ac7f464c69",
+    "url": "/list-order-app/static/js/runtime-main.5e4d67db.js"
+  },
+  {
+    "revision": "1c246b093f0b5fff45ea",
+    "url": "/list-order-app/static/js/stencil-h8k-logo_2-entry-js.1b687a10.chunk.js"
+  },
+  {
+    "revision": "49ce0cfcba0ea0f3c3ad",
+    "url": "/list-order-app/static/js/stencil-polyfills-css-shim.29e52000.chunk.js"
+  }
+]);
